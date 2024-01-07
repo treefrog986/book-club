a692c752eaf4989b05b38051c3bbc15aaf6145ee
Eli Sailer-Haugland <eli092011@gmail.com>
2024-01-07 15:03:25 -0600

    bug

--- app/dashboard/[book_id]/page.js
+++ app/dashboard/[book_id]/page.js
@@ -7,9 +7,9 @@ import { Button } from "@mui/material";
 
 export default function BookID(){
     const route = useRouter()
-    const {Book_id} = useParams()
+    const {book_id} = useParams()
     const {data} = useContext(AppContext)
-    const book = data.books?.filter(x=>x.id===parseInt(Book_id))[0]
+    const book = data.books?.filter(x=>x.id===parseInt(book_id))[0]
     return(book&&<>
     <p>Name : {book.title}</p>
     <p>Author : {book.author}</p>
