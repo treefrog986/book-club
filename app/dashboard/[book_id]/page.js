31477f0ef1f4208593323d7279e2707f77d97781
Eli Sailer-Haugland <eli092011@gmail.com>
2024-01-07 14:46:18 -0600

    bug

--- app/dashboard/[book_id]/page.js
+++ app/dashboard/[book_id]/page.js
@@ -5,11 +5,11 @@ import { useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
 import { Button } from "@mui/material";
 
-export default function bookID(){
+export default function BookID(){
     const route = useRouter()
-    const {book_id} = useParams()
+    const {Book_id} = useParams()
     const {data} = useContext(AppContext)
-    const book = data.books?.filter(x=>x.id===parseInt(book_id))[0]
+    const book = data.books?.filter(x=>x.id===parseInt(Book_id))[0]
     return(book&&<>
     <p>Name : {book.title}</p>
     <p>Author : {book.author}</p>
